79d4ed0e447bd52486b460ddd0da3793af7b00ac
hokage9211 <75352278+hokage9211@users.noreply.github.com>
2025-09-26 08:58:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
     res.redirect('/admin.html');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Load Cron Job (automatically schedules itself)
 // for replit
@@ -53,3 +53,4 @@ app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`))
 //         setTimeout(() => (toast.style.display = "none"), 3000);
 
 //       }
+
